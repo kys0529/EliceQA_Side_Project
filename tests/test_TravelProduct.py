97f6bc8091e3ba569c75f89f690014057c1d62bd
won95kim <hw95.kim@gmail.com>
2025-04-30 03:46:06 +0900

    [ADD] TravelProduct 관련 코드 추가
    대상 파일: page, Locator, test
    - 여행 패키지 페이지 진입 확인 테스트 함수 추가
    - 여행 패키지 페이지 UI요소 노출 확인 테스트 함수 추가
    - 여행 패키지 페이지 검색 확인 - 검색 결과 존재하는 케이스 테스트 함수 추가
    - 여행 패키지 페이지 검색 확인 - 검색 결과 존재하지 않는 케이스 테스트 함수 추가
    - 여행 패키지 페이지 전체 필터 확인 함수 추가(수정 예정)
    - 여행 패키지 페이지 필터 확인 (수정 예정) : parametrize 사용하여 10개 테스트 커버

--- tests/test_TravelProduct.py
+++ tests/test_TravelProduct.py
@@ -1,22 +1,161 @@
 import pytest
 from src.pages.TravelProduct import TravelProduct
-from appium.webdriver.common.appiumby import AppiumBy
+from src.utils.locators.TravelProductLocator import TravelProductListLocator
 from appium.webdriver.webdriver import WebDriver
-from src.utils.helpers import Helpers
+
 
 @pytest.mark.usefixtures("login_driver")
 class TestTP01:
-    def test_open(self, login_driver: WebDriver, request):
-        helpers = Helpers(login_driver, page_name="TravelProduct")
-        screenshot_file = helpers.setup_screenshot(request.node.name)
+    # [TP_01_01] 여행 패키지 페이지 진입 확인
+    def test_TP_01_01(self, login_driver: WebDriver, request):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
         
-        helpers.logger.info("접속 완료")
-        login_driver.save_screenshot(screenshot_file)
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
 
-    def test_tp_01_01(self, login_driver: WebDriver, request): # 기능/의도 설명
-        #travelProduct = TravelProduct() # 클래스에 대한 객체의 변수명은 첫 문자를 소문자로!
-        helpers = Helpers(login_driver, page_name="TravelProduct")
-        screenshot_file = helpers.setup_screenshot(request.node.name)
+            assert travelProduct.find_element(list_locators.PAGE_TITLE).is_displayed()
+            travelProduct.logger.info("여행 패키지 페이지 진입 확인")
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info("[TP_01_01] 여행 패키지 페이지 진입 확인 테스트 완료")
+
+
+    # [TP_01_02] 여행 패키지 페이지 UI요소 노출 확인
+    def test_TP_01_02(self, login_driver: WebDriver, request):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
+
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
         
-        helpers.logger.info("2차 접속 완료")
-        login_driver.save_screenshot(screenshot_file)
+            for ui_check in list_locators.UI_CHECK_ELEMENT:
+                assert travelProduct.find_element(ui_check).is_displayed()
+            travelProduct.logger.info("여행 패키지 페이지 UI요소 노출 확인")
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info("[TP_01_02] 여행 패키지 페이지 UI요소 노출 확인 테스트 완료")
+
+        
+class TestTP02:
+    # [TP_02_01] 여행 패키지 페이지 검색 확인 - 검색 결과 존재하는 케이스
+    def test_TP_02_01(self, login_driver: WebDriver, request):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
+
+        search_text = "강원"
+
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
+        
+            travelProduct.travelproduct_list_search(search_text)
+            travelProduct.logger.info(f"{search_text} 여행 패키지 검색")
+
+            assert travelProduct.find_element(list_locators.package_select(search_text)).is_displayed()
+            travelProduct.logger.info(f"{search_text} 여행 패키지 페이지 검색 확인 - 검색 결과 존재하는 케이스")
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info("[TP_02_01] 여행 패키지 페이지 검색 확인 - 검색 결과 존재하는 케이스 테스트 완료")
+
+
+    # [TP_02_02] 여행 패키지 페이지 검색 확인 - 검색 결과 존재하지 않는 케이스
+    def test_TP_02_02(self, login_driver: WebDriver, request):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
+
+        search_text = "프랑스"
+
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
+        
+            travelProduct.travelproduct_list_search(search_text)
+            travelProduct.logger.info(f"{search_text} 여행 패키지 검색")
+
+            for ui_check in list_locators.SEARCH_NONE_RESULT_CHECK:
+                assert travelProduct.find_element(ui_check).is_displayed()
+            travelProduct.logger.info(f"여행 패키지 페이지 검색 확인 - 검색 결과 존재하지 않는 케이스")
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info("[TP_02_02] 여행 패키지 페이지 검색 확인 - 검색 결과 존재하지 않는 케이스 테스트 완료")
+
+
+class TestTP03:
+    # [TP_03_01] 여행 패키지 페이지 전체 필터 확인
+    def test_TP_03_01(self, login_driver: WebDriver, request):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
+
+        filter_name = "전체"
+
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
+
+            # 비교용으로 패키지 저장 필요
+        
+            travelProduct.travelproduct_filter_select(filter_name)
+            travelProduct.logger.info(f"{filter_name} 필터 선택")
+            
+            # 비교용으로 패키지 저장 필요
+
+            # assert 문으로 비교 필요
+            travelProduct.logger.info(f"{filter_name} 전체 필터 확인")
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info("[TP_03_01] 여행 패키지 페이지 전체 필터 확인 테스트 완료")
+
+
+    # [TP_03_02~TP_03_11] 여행 패키지 페이지 필터 확인  
+    @pytest.mark.parametrize("filter_name", ["서울", "인천/경기", "강원", "대전/충남", "충북", "광주/전남", "전북", "부산/경남", "대구/경북", "제주도"])
+    def test_TP_03_02(self, login_driver: WebDriver, request, filter_name):
+        travelProduct = TravelProduct(login_driver)
+        list_locators = TravelProductListLocator()
+
+        try:
+            travelProduct.travelproduct_navi_click()
+            travelProduct.logger.info("여행 패키지 화면 진입")
+        
+            travelProduct.travelproduct_filter_select(filter_name)
+            travelProduct.logger.info(f"{filter_name} 필터 선택")
+
+            assert travelProduct.find_element(list_locators.filter_select(filter_name)).is_displayed()
+            travelProduct.logger.info(f"{filter_name} 필터 확인")
+
+            # if문 이용해서 패키지 없으면 등록된 패키지 문구 확인 하는 기능 추가
+
+        except Exception as e:
+            travelProduct.logger.error(f"✖ 테스트 중 문제 발생: {e}")
+            travelProduct.save_screenshot(request.node.name)
+            raise
+
+        finally:
+            travelProduct.logger.info(f"[TP_03_02~TP_03_11] 여행 패키지 페이지 {filter_name} 필터 확인  테스트 완료")
