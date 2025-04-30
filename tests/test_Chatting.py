29bb18d26d2672d81403ee8d119a55d3f7b219d1
sey <hok4070@gmail.com>
2025-04-30 10:44:49 +0900

    Revert "0428 마무리"
    This reverts commit a8e7d602daa32afa0120d68f4a121694399aa7d9.

--- tests/test_Chatting.py
+++ tests/test_Chatting.py
@@ -7,13 +7,13 @@ from utils.locators import ChattingLocator
 
 @pytest.mark.usefixtures("login_driver")
 class TestTP01:
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_tp_01_01(self, login_driver: WebDriver, request): # 채팅 탭 진입
 
         chatting = Chatting(login_driver)
         chatting.into_chatting_tap()
     
-        chat_tab_element = chatting.find_element(ChattingLocator.CHATTING_TAB_ICON)
+        chat_tab_element = chatting.find_element(ChattingLocator.CHATTING_TAB)
         selected_status = chat_tab_element.get_attribute('selected')
         assert selected_status == 'true', '❌ 채팅 탭 진입 실패'
 
@@ -21,21 +21,18 @@ class TestTP01:
         chatting.save_screenshot(request.node.name)
 
 
-    # @pytest.mark.skip()
+    @pytest.mark.skip()
     def test_tp_01_02(self, login_driver: WebDriver, request): # 채팅 탭 UI요소 확인
 
         chatting = Chatting(login_driver)
-        chatting.into_chatting_tap()
+        chatting.into_chatting_tap(self)
     
         title_element = chatting.find_element(ChattingLocator.CHATTING_TAB_TITLE)
         title = title_element.get_attribute('content-desc')
         assert title == '채팅 목록', '❌ 채팅 목록 타이틀이 보이지 않음'
 
-        search_input = chatting.find_element(ChattingLocator.SEARCH_INPUT_CHAT)
+        search_input = chatting.find_element(ChattingLocator.SEARCH_INPUT)
         assert search_input.is_displayed(), '❌ 검색창 보이지 않음'
-
-        search_btn = chatting.find_element(ChattingLocator.SEARCH_BTN_CHAT)
-        assert search_btn.is_displayed(), '❌ 검색버튼이 보이지 않음'
 
         chatting.logger.info("✅ 채팅 탭 UI요소 확인")
         chatting.save_screenshot(request.node.name)
