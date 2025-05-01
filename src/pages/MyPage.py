8e000f09ac90d3bb41a4e27a0705dfc0a2922e96
YANA <ysi1246@daum.net>
2025-05-02 01:49:33 +0900

    [UPDATE] MyPage.py / MyPageLocator.py / MyPage.py
    # MyPage.py
    - 메서드 신규 추가
      - 로그인 후 닉네임 값 받아오기
      - 마이페이지 탭 진입
      - 프로필 페이지 진입
    
    # MyPageLocator.py
    - 'Locator' 단어가 포함된 Class명에서 해당 단어 삭제
    - 변수명 간소화
    - 신규 필요 로케이터 값 추가
    
    # test_MyPage.py
    - 기 작성 TC 내용 수정
    - 신규 테스트 클래스 및 함수 추가
    - TC 내용 주석으로 Precondition / Steps / Expected Result 구분

--- src/pages/MyPage.py
+++ src/pages/MyPage.py
@@ -1,5 +1,21 @@
 from src.pages.BasePage import BasePage
+from src.utils.locators import MyPageLocator
+
 
 class MyPage(BasePage):
     def __init__(self, driver, page_name="MyPage"):
         super().__init__(driver, page_name)
+
+    def get_nickname(self):  # 로그인 후 닉네임 값 받아오기
+        current_user_nickname = self.get_attribute(
+            MyPageLocator.MypageMain.LOGIN_TOAST, "content-desc"
+        ).replace(
+            "님 환영합니다!", ""
+        )  # 토스트 내용에서 닉네임 영역만 남김
+        return current_user_nickname
+
+    def into_mypage(self):  # 마이페이지 탭 진입
+        self.click_element(MyPageLocator.MypageMain.MYPAGE_TAB)
+
+    def into_profile_page(self):  # 프로필 페이지 진입
+        self.click_element(MyPageLocator.MypageMain.PROFILE_PHOTO)
--- tests/test_MyPage.py
+++ tests/test_MyPage.py
@@ -2,6 +2,7 @@ import pytest
 from appium.webdriver.webdriver import WebDriver
 from src.pages.MyPage import MyPage
 from src.utils.locators import MyPageLocator
+import time
 
 # class TestMP00:
 #     def test_mp_00_00(): # 기능/의도 설명
@@ -9,13 +10,149 @@ from src.utils.locators import MyPageLocator
 
 
 @pytest.mark.usefixtures("login_driver")
-class TestMP01: # 홈 탭 진입 상태 > 마이페이지 진입 가능 여부 확인
-    def test_mp_01_01(self, login_driver:WebDriver, request):
+class TestMP01:
+    @pytest.mark.done
+    def test_mp_01_01(self, login_driver: WebDriver, request):  # 홈 탭 진입 상태 > 마이페이지 탭 진입 가능 여부 확인
         try:
-            mypage = MyPage()
-            mypage.driver.find_element(*MyPageLocator.MypageMainLocator.MYPAGE_TAB).click()
-            assert mypage.find_element(*MyPageLocator.MypageMainLocator.MYPAGE_TITLE).is_displayed()
+            # Precondition
+            mypage = MyPage(login_driver)
+
+            # Steps
+            mypage.into_mypage()
+
+            # Expected Result
+            assert mypage.find_element(MyPageLocator.MypageMain.MYPAGE_TITLE)
             mypage.logger.info("✅ 홈 탭 진입 상태에서 마이페이지 진입 확인")
+
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+    @pytest.mark.done
+    def test_mp_01_02(self, login_driver: WebDriver, request):  # 여행상품 탭 진입 상태 > 마이페이지 탭 진입 가능 여부 확인
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.click_element(MyPageLocator.MypageMain.TRAVEL_PRODUCT_TAB)
+
+            # Steps
+            mypage.into_mypage()
+
+            # Expected Result
+            assert mypage.find_element(MyPageLocator.MypageMain.MYPAGE_TITLE)
+            mypage.logger.info("✅ 여행상품 탭 진입 상태에서 마이페이지 진입 확인")
+
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+    @pytest.mark.done
+    def test_mp_01_03(self, login_driver: WebDriver, request):  # 채팅 탭 진입 상태 > 마이페이지 탭 진입 가능 여부 확인
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.click_element(MyPageLocator.MypageMain.CHATTING_TAB)
+
+            # Steps
+            mypage.into_mypage()
+
+            # Expected Result
+            assert mypage.find_element(MyPageLocator.MypageMain.MYPAGE_TITLE)
+            mypage.logger.info("✅ 채팅 탭 진입 상태에서 마이페이지 진입 확인")
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+    @pytest.mark.done
+    def test_mp_01_04(self, login_driver: WebDriver, request):  # 마이페이지 탭 진입 상태 > 마이페이지 탭 선택 시 진입 상태 유지 확인
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.into_mypage()
+            time.sleep(1)  # 마이페이지 진입 후 아주 잠시 대기
+
+            # Steps
+            mypage.into_mypage()
+
+            # Expected Result
+            assert mypage.find_element(MyPageLocator.MypageMain.MYPAGE_TITLE)
+            mypage.logger.info(
+                "✅ 마이페이지 탭 진입 상태에서 마이페이지 진입 상태 유지 확인"
+            )
+
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+
+@pytest.mark.skip("스크린샷으로 비교 필요로, 검증 과정 추후 개발 예정")
+@pytest.mark.usefixtures("login_driver")
+class TestMP02:
+    @pytest.mark.wip
+    def test_mp_02_01(self, login_driver: WebDriver, request):  # 앱 테마 변경 - 다크 모드
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.into_mypage()
+
+            # Steps
+            mypage.click_element(MyPageLocator.MypageMain.THEME_TOGGLE_BTN)
+
+            # Expected Result
+            '''assert 추가 필요'''
+            mypage.logger.info("✅ 앱 테마 - 다크 모드로 변경 확인")
+
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+    @pytest.mark.wip
+    def test_mp_02_02(self, login_driver: WebDriver, request):  # 앱 테마 변경 - 라이트 모드
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.into_mypage()
+
+            # Steps
+            mypage.click_element(MyPageLocator.MypageMain.THEME_TOGGLE_BTN)
+            mypage.click_element(MyPageLocator.MypageMain.THEME_TOGGLE_BTN)
+
+            # Expected Result
+            '''assert 추가 필요'''
+            mypage.logger.info("✅ 앱 테마 - 라이트 모드로 변경 확인")
+
+        except Exception as e:
+            mypage.logger.info(f"❌ 테스트 실패: {e}")
+            mypage.save_screenshot(request.node.name)
+            raise
+
+
+@pytest.mark.usefixtures("login_driver")
+class TestMP03:
+    def test_mp_03_01(self, login_driver: WebDriver, request):  # 마이페이지 > 프로필 페이지 진입 여부 확인
+        try:
+            # Precondition
+            mypage = MyPage(login_driver)
+            mypage.into_mypage()
+            nickname = mypage.get_nickname()  # 현재 로그인 한 유저의 닉네임 받아오기
+            mypage.logger.info(f"✅ 현재 로그인 한 유저 닉네임 : {nickname}")
+
+            # Steps
+            mypage.click_element(MyPageLocator.MypageMain.PROFILE_PHOTO)
+
+            PROFILE_PAGE_TITLE = MyPageLocator.MypageProfile.get_profile_title_locator(nickname)  # 프로필 페이지 타이틀 로케이터 완성
+
+            profile_title_text = mypage.get_attribute(PROFILE_PAGE_TITLE, "content-desc")
+
+            # Expected Result
+            assert profile_title_text == f"{nickname} 님의 프로필"
+            mypage.logger.info("✅ 마이페이지에서 프로필 페이지 정상 진입 확인")
+
         except Exception as e:
             mypage.logger.info(f"❌ 테스트 실패: {e}")
             mypage.save_screenshot(request.node.name)
