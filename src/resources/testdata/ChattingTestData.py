d7ccbfabc932be4e25527f8774795b379587ffde
sey <hok4070@gmail.com>
2025-05-05 18:12:38 +0900

    Create ChattingTestData.py

---
+++ src/resources/testdata/ChattingTestData.py
@@ -0,0 +1,21 @@
+chat_users = {
+    "user_name": "사용자 B",
+    "email": "mouffixummoiteu-2296@yopmail.com",
+}
+
+data_package = {
+  "package_name": '빵순이',
+  "invaild_text": '!@#' 
+}
+
+input_text = {
+  "text": "테스트",
+  "long_text": "슝" * 500
+}
+
+data_location = {
+  "location_name": '성심당 본점',
+  "location_address": '대전광역시 중구 은행동 145',
+  "invaild_text": '###@@!',
+  "not_exist_name": '꿂'
+}
